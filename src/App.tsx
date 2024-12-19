0377b19e58c4485f4764213417d895d89ef3445d
cnfc29 <cnfc629@gmail.com>
2024-12-19 21:23:21 +0300

    fix 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
-  const routes = [{ path: "/weather/", element: <MainPage /> }];
-
-  const router = createBrowserRouter(routes);
-
-  return <RouterProvider router={router} />;
+  return <MainPage />;
 }
 
 export default App;
